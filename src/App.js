524d81f0f23b9acafbbc0790248bd3791775c536
Tejendra <tejendrasrajawat@gmail.com>
2021-10-20 09:45:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,54 @@
 import './App.css';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import ToDoList from './ToDoList';
+
+const getLocalItems = () =>{
+let list = localStorage.getItem('list');
+if(list){
+    return JSON.parse(localStorage.getItem('list'))
+} else {
+    return [] ;
+}
+}
+
 
 function App(){
 
-    const [inputItem, setInputItem] = useState();
-    const [Items, setItems] = useState([]);
+    const [inputItem, setInputItem] = useState('');
+    const [Items, setItems] = useState(getLocalItems());
 // 
     const itemChanger = (event)  => {
-        console.log("put value in input!");
+        // console.log("put value in input!");
         setInputItem(event.target.value);
     };
 // 
     const addItem = () => {
-        console.log("Item added in spread array!");
+        // console.log("Item added in spread array!");
         setItems((oldItems) => {
             return [...oldItems, inputItem];
         });
         setInputItem("");
     };
 // 
+const onEnterClick = e => {
+    if(e.code === "Enter" || e.code === "NumpadEnter"){
+        addItem();
+    }
+}   
+// 
     const deleteItems = (id) => {
-        console.log("deleted!");
+        // console.log("deleted!");
         setItems((oldItems)=> {
             return oldItems.filter((arrElem, index)=>{
                 return index !== id;
             })
         })
     }   
+// 
+    useEffect(()=>{
+    localStorage.setItem('list', JSON.stringify(Items));
+    },[Items]);
+
     return(
        <>
        <div className="main-div">
@@ -35,8 +56,8 @@ function App(){
            <br/>
         <h1>To-do List</h1>
            <br/>
-        <input type="text" placeholder="Enter Your Todo 😀" value={inputItem} onChange={itemChanger} />
-        <button onClick={addItem}> + </button>
+        <input type="text" placeholder="Enter Your Todo 😀" value={inputItem} onChange={itemChanger} onKeyPress={onEnterClick}/>
+        <button onClick={addItem} > + </button>
 
         <ol>
            
