4f0cdb5fed71cdba8bb8cf17ec6134a50a3c5fe8
Tejendra <Tejendrasrajawat@gmail.com>
2023-02-21 10:30:35 +0530

    new functionality added

--- src/App.js
+++ src/App.js
@@ -1,79 +1,99 @@
-import './App.css';
-import React, {useEffect, useState} from 'react';
-import ToDoList from './ToDoList';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import ToDoList from "./ToDoList";
 
-const getLocalItems = () =>{
-let list = localStorage.getItem('list');
-if(list){
-    return JSON.parse(localStorage.getItem('list'))
-} else {
-    return [] ;
-}
-}
+const getLocalItems = () => {
+  let list = localStorage.getItem("list");
+  if (list) {
+    return JSON.parse(localStorage.getItem("list"));
+  } else {
+    return [];
+  }
+};
 
+function App() {
+  const [inputItem, setInputItem] = useState("");
+  const [Items, setItems] = useState(getLocalItems());
 
-function App(){
+  const itemChanger = (event) => {
+    setInputItem(event.target.value);
+  };
 
-    const [inputItem, setInputItem] = useState('');
-    const [Items, setItems] = useState(getLocalItems());
-// 
-    const itemChanger = (event)  => {
-        // console.log("put value in input!");
-        setInputItem(event.target.value);
-    };
-// 
-    const addItem = () => {
-        // console.log("Item added in spread array!");
-        setItems((oldItems) => {
-            return [...oldItems, inputItem];
-        });
-        setInputItem("");
-    };
-// 
-const onEnterClick = e => {
-    if(e.code === "Enter" || e.code === "NumpadEnter"){
-        addItem();
+  const addItem = () => {
+    if (inputItem.trim() !== "") {
+      setItems((oldItems) => {
+        return [...oldItems, { value: inputItem, isChecked: false }];
+      });
+      setInputItem("");
     }
-}   
-// 
-    const deleteItems = (id) => {
-        // console.log("deleted!");
-        setItems((oldItems)=> {
-            return oldItems.filter((arrElem, index)=>{
-                return index !== id;
-            })
-        })
-    }   
-// 
-    useEffect(()=>{
-    localStorage.setItem('list', JSON.stringify(Items));
-    },[Items]);
+  };
 
-    return(
-       <>
-       <div className="main-div">
-       <div className="center-div">
-           <br/>
-        <h1>To-do List</h1>
-           <br/>
-        <input type="text" placeholder="Enter Your Todo 😀" value={inputItem} onChange={itemChanger} onKeyPress={onEnterClick}/>
-        <button onClick={addItem} > + </button>
+  const onEnterClick = (e) => {
+    if (e.code === "Enter" || e.code === "NumpadEnter") {
+      addItem();
+    }
+  };
 
-        <ol>
-           
-           { Items.map( (itemval, index) => {
-               return <ToDoList 
-               key={index} 
-               id={index} 
-               text= {itemval}
-               onSelect = {deleteItems} />
-            } ) }
-        </ol>
-       </div>
-       </div>
-       
-       </>
-    )
+  const deleteItems = (id) => {
+    setItems((oldItems) => {
+      return oldItems.filter((arrElem, index) => {
+        return index !== id;
+      });
+    });
+  };
+
+  const deleteChecked = () => {
+    setItems((oldItems) => {
+      return oldItems.filter((item, index) => {
+        return item.isChecked === false;
+      });
+    });
+  };
+
+  const update = (newItems) => {
+    setItems(newItems);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(Items));
+  }, [Items]);
+
+  return (
+    <>
+      <div className="main-div">
+        <div className="center-div">
+          <br />
+          <h1>To-do List</h1>
+          <br />
+          <input
+            type="text"
+            placeholder="Enter Your Todo 📑"
+            value={inputItem}
+            onChange={itemChanger}
+            onKeyPress={onEnterClick}
+          />
+          <button onClick={addItem}> + </button>
+
+          <ol>
+            {Items.map((itemval, index) => {
+              return (
+                <ToDoList
+                  key={index}
+                  id={index}
+                  text={itemval}
+                  onSelect={deleteItems}
+                  update={update}
+                />
+              );
+            })}
+          </ol>
+          <p className="clearAll" onClick={deleteChecked}>
+            Clear Completed
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,19 +1,59 @@
-const ToDoLists = (props) => {
+import React, { useEffect, useState } from "react";
 
-   
+const ToDoLists = ({ onSelect, id, text, update }) => {
+  const [editable, setEditable] = useState(false);
+  const [newInputItem, setNewInputItem] = useState(text.value);
 
-    return (
-        <>
-        <div className="todo-style">
-        <i className="fa fa-times" aria-hidden="true"
-        onClick = { () => {
-        props.onSelect(props.id);
-        }}/>
-        <li>{props.text}</li>    
-        </div>
-        </>
-    ) 
-    
+  const updateValue = () => {
+    setEditable(false);
+    let newItems = JSON.parse(localStorage.getItem("list"));
+    newItems[id] = { value: newInputItem, isChecked: false };
+    update(newItems);
+  };
+
+  const onChecked = () => {
+    let newItems = JSON.parse(localStorage.getItem("list"));
+    newItems[id] = {
+      value: newItems[id].value,
+      isChecked: newItems[id].isChecked === true ? false : true,
+    };
+    update(newItems);
+  };
+
+  useEffect(() => {
+    setNewInputItem(text.value);
+  }, [text]);
+
+  return (
+    <>
+      <div className="todo-style">
+        <p className="btn" aria-hidden="true" onClick={() => onSelect(id)}>
+          x
+        </p>
+        <p className="btn" aria-hidden="true" onClick={() => onChecked(id)}>
+          {text.isChecked ? "⭕" : "✅"}
+        </p>
+        {editable ? (
+          <>
+            <input
+              value={newInputItem}
+              onChange={(e) => setNewInputItem(e.target.value)}
+            />
+            <button onClick={updateValue}>+</button>
+          </>
+        ) : (
+          <li
+            onClick={() => setEditable(true)}
+            style={{
+              textDecoration: text.isChecked === true ? "line-through" : "none",
+            }}
+          >
+            {text.value}
+          </li>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default ToDoLists;
